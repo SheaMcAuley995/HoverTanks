3786469105a9471295a7f35965ab3223ff773fd7
Shea McAuley <s179049@students.aie.edu.au>
2018-04-18 12:04:44 -0700

    school push

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Gun : MonoBehaviour {
+public class Target : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
